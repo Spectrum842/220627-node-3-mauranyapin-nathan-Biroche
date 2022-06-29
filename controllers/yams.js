ce4ea9822c25a77a4fdba3d581e1317e89eb3e4b
Spectrum842 <mauranyapin.nathan@gmail.com>
2022-06-29 16:30:22 +0200

    Suite règles yams

--- controllers/yams.js
+++ controllers/yams.js
@@ -7,13 +7,12 @@ const { APP_SECRET: secret } = process.env
 export const YamsControllerGet = (req, res) => {
 
     let diceTab = []
-    let patrieTab = []
     let patrieWin = []
     let occurenceTab = []
     console.log(req.query)
     PatrieModel.find()
         .then((patrieTab) =>{
-            console.log(patrieTab[0]._id);
+            //console.log(patrieTab[0]._id);
             if(req.query.play == "true"){
                 console.log('coucou')
                 
@@ -22,22 +21,27 @@ export const YamsControllerGet = (req, res) => {
                     diceTab[i]= (Math.floor(Math.random()*6 +1 ))
                 }
                 console.log(diceTab)
-                let occurence1 = diceTab.filter(element => element==1);
-                let occurence2 = diceTab.filter(element => element==2);
-                let occurence3 = diceTab.filter(element => element==3);
-                let occurence4 = diceTab.filter(element => element==4);
-                let occurence5 = diceTab.filter(element => element==5);
-                let occurence6 = diceTab.filter(element => element==6);
-                occurenceTab.push(occurence1.length)
-                occurenceTab.push(occurence2.length)
-                occurenceTab.push(occurence3.length)
-                occurenceTab.push(occurence4.length)
-                occurenceTab.push(occurence5.length)
-                occurenceTab.push(occurence6.length)
+                for(let i= 1; i<7; i++){
+                    occurenceTab.push(diceTab.filter(element => element==i).length)
+                }
                 console.log(occurenceTab)
+                if(occurenceTab.filter(element => element == 2).length>1){
+                    console.log('Double paire !')
+                    
+                    console.log(patrieTab[(Math.floor(Math.random()*8 ))])
+                    patrieWin.push(patrieTab[(Math.floor(Math.random()*8 ))])
+                    
+                    
+                }else if(occurenceTab.filter(element => element == 4) != ''){
+                    console.log('Carré ! ')
+                        patrieWin.push(patrieTab[(Math.floor(Math.random()*8 ))])
+                        patrieWin.push(patrieTab[(Math.floor(Math.random()*8 ))])
+                }else{
+                    console.log("Perdu l'ami")
+                }
             }
             
-            res.render('../views/game/yams', {diceTab}) 
+            res.render('../views/game/yams', {diceTab, patrieWin}) 
     } );
 }
 
